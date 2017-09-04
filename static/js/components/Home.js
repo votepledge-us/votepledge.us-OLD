03ab1ff40f8532085dc5a445e6c96d263cd50ae8
ISABELLE <isabelle.k.miller@gmail.com>
2017-09-04 11:41:20 -0700

    configured webpack with entrypoint app.js, added basic home component & css file

---
+++ static/js/components/Home.js
@@ -0,0 +1,17 @@
+require('../../styles/main.scss')
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>
+        You have entered the Home component
+      </div>
+    )
+  }
+}
+
+export default Home
