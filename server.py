8625b3c79084a0d9f86f0bc82771c272a5ba8b01
njhalq <31521029+njhalq@users.noreply.github.com>
2017-09-05 18:51:48 -0400

    Adds a few routes
    This is the basic framework I envision for the site. Templates and JS files can be developed with this set of routes and their relationships in mind.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template, redirect, request, flash, ses
 from flask_debugtoolbar import DebugToolbarExtension 
 from flask_sqlalchemy import SQLAlchemy # ORM for PostgreSQL database
 import os
+#from googleapiclient.civicinfo.representatives import representativesByAddress
 
 GOOGLE_API_KEY = os.environ['google_api_key'] # source google api key from os environment
 
@@ -14,9 +15,36 @@ def index():
     """ Landing. """
     return render_template('index.html')
 
+# Below is a Flask App framework I envision for this site.
+# Template files referenced don't yet exist.
 
+"""
+@app.route('/enterinfo') # Interface for when user clicks 'Get Started'
+def enterinfo():
+    # template will contain fields for a user's name and zipcode
+    render_template('enterinfo.html')
 
+@app.route('/repinfo')
+def repinfo():
+    addr = request.form['zipcode'] # gets user's zipcode from '/enterinfo'
+    name = request.form['name'] # gets user's name from '/enterinfo'
+    API_call = representativeInfoByAddress(levels='country', roles='legislatorLowerBody',
+                                           address=addr)
+    rep=API_call['officials'][0]['name']
+    party=API_call['officials'][0]['party'] # different template will render depending on party
+    if party == 'Republican':
+        render_template('repinfoGOP.html', name=name, rep=rep) # name will be submitted to next page
+    elif party == 'Democratic':
+        render_template('repinfoDEM.html', name=name, rep=rep)
 
+@app.route('/pledge')
+def pledge():
+    name = request.args.get('name', None)
+    rep = request.args.get('rep', None)
+    if request.method == 'POST':
+        # send submitted data to backend
+    render_template('pledge.html', name=name, rep=rep)
+""" 
 
 if __name__ == "__main__":
     app.debug = True
