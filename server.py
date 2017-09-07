371768a4d2e02b16d71c62f563e7abc1fb8dd479
njhalq <31521029+njhalq@users.noreply.github.com>
2017-09-07 18:44:32 -0400

    prep to run on localhost

--- server.py
+++ server.py
@@ -8,14 +8,14 @@ from googleapiclient.discovery import build
 # GOOGLE_API_KEY = os.environ['google_api_key'] # source google api key from os environment
 GOOGLE_API_KEY = 'AIzaSyBO9CwLQzVNB_dZUUEcsHHFKBkfoqOi8PQ'
 app = Flask(__name__) # create instance of Flask app
-app.secret_key = 'SECRET_KEY'
-app.config['SECRET_KEY']
-
+#app.secret_key = 'SECRET_KEY'
+#app.config['SECRET_KEY']
+"""
 @app.route('/')
 def index():
     """ Landing. """
     return render_template('index.html')
-
+"""
 # Below is a Flask App framework I envision for this site.
 
 @app.route('/enterinfo') # Interface for when user clicks 'Get Started'
@@ -52,6 +52,6 @@ def pledge():
 
 
 if __name__ == "__main__":
-    app.debug = True
-    DebugToolbarExtension(app) # creates toolbar extension
-    app.run(port=5000, host='0.0.0.0')
+    #app.debug = True
+    #DebugToolbarExtension(app) # creates toolbar extension
+    app.run(debug=True)
